4b2d1242df54108f6855b1b69be86026cab6e0b7
Marco5074 <00114825@uca.edu.sv>
2026-04-03 17:47:54 -0600

    :sparkles: feat: Agregar atributos básicos a la clase Elemento

--- Proyecto_Prueba/src/main/java/modelo/Elemento.java
+++ Proyecto_Prueba/src/main/java/modelo/Elemento.java
@@ -2,5 +2,11 @@ package modelo;
 
 public class Elemento
 {
+    private int id;
+    private String titulo;
+    public TipoElemento tipo;
+    private String descripcion;
+    private String nivelPrioridad;
+
 
 }
