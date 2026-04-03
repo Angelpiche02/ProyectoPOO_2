28593d677deaf24cd250b0ed1d6e03477fd5c0c2
Marco5074 <00114825@uca.edu.sv>
2026-04-03 17:54:08 -0600

    :sparkles: feat: Implementar setters en clase Elemento

--- Proyecto_Prueba/src/main/java/modelo/Elemento.java
+++ Proyecto_Prueba/src/main/java/modelo/Elemento.java
@@ -18,6 +18,13 @@ public class Elemento
         this.nivelPrioridad = nivelPrioridad;
     }
 
+    public void setId(int id){this.id = id;}
+    public void setTitulo(String titulo){this.titulo = titulo;}
+    public void setTipo(TipoElemento tipo){this.tipo = tipo;}
+    public void setDescripcion(String descripcion){this.descripcion = descripcion;}
+    public void setNivelPrioridad(String nivelPrioridad){this.nivelPrioridad = nivelPrioridad;}
+
+
 
 
 }
