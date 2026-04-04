34251dccf666669040992ceaadcd4bf00bdc773a
rivasfer741 <rivasfer741@gmail.com>
2026-04-03 22:16:41 -0600

    feat: agregar repetitividad y frecuencia en Tarea

--- Proyecto_Prueba/src/main/java/modelo/SubClaseTarea.java
+++ Proyecto_Prueba/src/main/java/modelo/SubClaseTarea.java
@@ -6,22 +6,36 @@ public class Tarea extends Elemento
 {
     private String estado;
     private String categoria;
+    private boolean esRepetitiva;
+    private Frecuencia frecuencia;
 
+    public enum Frecuencia
+    {
+        DIARIA, SEMANAL, MENSUAL
+    }
+
+    //constructor
     public Tarea() {}
     public Tarea(int id, String titulo, TipoElemento tipo, String descripcion, String nivelPrioridad, String estado, String categoria)
     {
         super(id, titulo, tipo, descripcion, nivelPrioridad);
         this.estado = estado;
         this.categoria = categoria;
+        this.esRepetitiva = false;
+        this.frecuencia = null;
     }
 
     //Metodos get
     public String getEstado(){return this.estado;}
     public String getCategoria(){return this.categoria;}
+    public boolean esRepetitiva(){return this.esRepetitiva;}
+    public Frecuencia getFrecuencia(){return this.frecuencia;}
 
     //Metodos set
     public void setEstado(String estado){this.estado = estado;}
     public void setCategoria(String categoria){this.categoria = categoria;}
+    public void setEsRepetitiva(boolean esRepetitiva){this.esRepetitiva = esRepetitiva;}
+    public void setFrecuencia(Frecuencia frecuencia){this.frecuencia = frecuencia;}
 
     public static List<Elemento> obtenerTareas()
     {
