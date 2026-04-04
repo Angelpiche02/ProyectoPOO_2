759e0a0ddb1f35f07fb51c2f8243904b1a1a6d8f
Marco5074 <00114825@uca.edu.sv>
2026-04-03 22:42:06 -0600

    :sparkles: feat: Agregar métodos constructores en Tarea

--- Proyecto_Prueba/src/main/java/modelo/Tarea.java
+++ Proyecto_Prueba/src/main/java/modelo/Tarea.java
@@ -7,5 +7,14 @@ public class Tarea extends Elemento
 {
     private String estado;
     private String categoria;
-    
+
+    public Tarea() {}
+    public Tarea(int id, String titulo, TipoElemento tipo, String descripcion, String nivelPrioridad, String estado, String categoria)
+    {
+        super(id, titulo, tipo, descripcion, nivelPrioridad);
+        this.estado = estado;
+        this.categoria = categoria;
+    }
+
+
 }
