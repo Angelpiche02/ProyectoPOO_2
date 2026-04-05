85ff6e08b1005cdf3cc08f541ab8794fd6e37b08
Marco5074 <00114825@uca.edu.sv>
2026-04-05 11:16:47 -0600

    :sparkles: feat: agregar métodos para GestorRecordatorio

--- Proyecto_Prueba/src/main/java/services/GestorRecordatorio.java
+++ Proyecto_Prueba/src/main/java/services/GestorRecordatorio.java
@@ -1,6 +1,36 @@
 package services;
+import modelo.Recordatorio;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GestorRecordatorio
 {
+    private List<Recordatorio> recordatorios;
+
+    public GestorRecordatorio() {
+        recordatorios = new ArrayList<>();
+    }
+
+    // Agregar recordatorio
+    public void agregarRecordatorio(Recordatorio r) {
+        recordatorios.add(r);
+        System.out.println("Recordatorio agregado con éxito");
+    }
+
+    // Mostrar todos los recordatorios
+    public void mostrarRecordatorios() {
+
+        System.out.println("\nLista de recordatorios:");
+
+        if (recordatorios.isEmpty()) {
+            System.out.println("No hay recordatorios registrados.");
+            return;
+        }
+
+        for (Recordatorio r : recordatorios) {
+            r.mostrarElemento();
+        }
+    }
 
 }
