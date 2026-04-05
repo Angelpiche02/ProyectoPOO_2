e248ddf56a384135dd0740e00b1586c6dd8047cc
Marco5074 <00114825@uca.edu.sv>
2026-04-05 11:05:15 -0600

    :sparkles: feat: agregar métodos para GestorTarea

---
+++ Proyecto_Prueba/src/main/java/services/GestorTarea.java
@@ -0,0 +1,37 @@
+package services;
+import modelo.Tarea;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class GestorTarea
+{
+    private List<Tarea> tareas;
+
+    public GestorTarea() {
+        tareas = new ArrayList<>();
+    }
+
+    // Agregar tarea
+    public void agregarTarea(Tarea t) {
+        tareas.add(t);
+        System.out.println("Tarea agregada con éxito");
+    }
+
+    // Mostrar todas las tareas
+    public void mostrarTareas() {
+
+        System.out.println("\nListado de tareas: ");
+
+        if (tareas.isEmpty()) {
+            System.out.println("No hay tareas registradas.");
+            return;
+        }
+
+        for (Tarea t : tareas) {
+            t.mostrarElemento();
+        }
+    }
+
+}
